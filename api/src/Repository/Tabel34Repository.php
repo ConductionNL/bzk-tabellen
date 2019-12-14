9ad66b769c2331ae99d873ad06efd9dcc7a8b6c2
Ruben van der Linde <ruben@conduction.nl>
2019-12-14 01:47:40 +0100

    Fixes on repros

--- api/src/Repository/Tabel34Repository.php
+++ api/src/Repository/Tabel34Repository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Tabel34;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Tabel34|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class Tabel34Repository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+	public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Tabel34::class);
     }
--- api/src/Repository/Tabel41Repository.php
+++ api/src/Repository/Tabel41Repository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Tabel41;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Tabel41|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class Tabel41Repository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+	public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Tabel41::class);
     }
