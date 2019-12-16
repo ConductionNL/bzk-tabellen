25273ca96429bae9a66f9ca251697fa3b27e4756
Ruben van der Linde <ruben@conduction.nl>
2019-12-16 08:54:03 +0100

    Fixy?

--- api/src/Repository/Tabel55Repository.php
+++ api/src/Repository/Tabel55Repository.php
@@ -19,32 +19,4 @@ class Tabel55Repository extends ServiceEntityRepository
         parent::__construct($registry, Tabel55::class);
     }
 
-    // /**
-    //  * @return Tabel32[] Returns an array of Tabel32 objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('t.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Tabel32
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
